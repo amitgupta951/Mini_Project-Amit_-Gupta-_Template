89eef5848fd3b753bf0fb684efc28232055cbaf2
Amit Gupta <amitintern82@gmail.com>
2023-11-21 11:26:34 +0530

    Amit:

--- index.js
+++ index.js
@@ -1,72 +1,71 @@
-const myslide = document.querySelectorAll('.myslide'),
-	  dot = document.querySelectorAll('.dot');
+const myslide = document.querySelectorAll(".myslide"),
+  dot = document.querySelectorAll(".dot");
 let counter = 1;
-slidefun(counter);
+
+showSlides(counter); // Call the initial slide display
 
 let timer = setInterval(autoSlide, 8000);
+
 function autoSlide() {
-	counter += 1;
-	slidefun(counter);
+  counter += 1;
+  showSlides(counter);
 }
+
 function plusSlides(n) {
-	counter += n;
-	slidefun(counter);
-	resetTimer();
+  counter += n;
+  showSlides(counter);
+  resetTimer();
 }
+
 function currentSlide(n) {
-	counter = n;
-	slidefun(counter);
-	resetTimer();
+  counter = n;
+  showSlides(counter);
+  resetTimer();
 }
+
 function resetTimer() {
-	clearInterval(timer);
-	timer = setInterval(autoSlide, 8000);
+  clearInterval(timer);
+  timer = setInterval(autoSlide, 8000);
 }
 
-function slidefun(n) {
-	
-	let i;
-	for(i = 0;i<myslide.length;i++){
-		myslide[i].style.display = "none";
-	}
-	for(i = 0;i<dot.length;i++) {
-		dot[i].className = dot[i].className.replace(' active', '');
-	}
-	if(n > myslide.length){
-	   counter = 1;
-	   }
-	if(n < 1){
-	   counter = myslide.length;
-	   }
-	myslide[counter - 1].style.display = "block";
-	dot[counter - 1].className += " active";
+function showSlides(n) {
+  let i;
+  for (i = 0; i < myslide.length; i++) {
+    myslide[i].style.display = "none";
+  }
+  for (i = 0; i < dot.length; i++) {
+    dot[i].className = dot[i].className.replace(" active", "");
+  }
+  if (n > myslide.length) {
+    counter = 1;
+  }
+  if (n < 1) {
+    counter = myslide.length;
+  }
+  myslide[counter - 1].style.display = "block";
+  dot[counter - 1].className += " active";
 }
-
 
 const loginPopup = document.querySelector(".login-popup");
 const close = document.querySelector(".close");
 
+window.addEventListener("load", function () {
+  showPopup();
+});
 
-window.addEventListener("load",function(){
-
- showPopup();
-})
-
-function showPopup(){
-	  const timeLimit = 3    // seconds;
-	  let i=0;
-	  const timer = setInterval(function(){
-	   i++;
-	   if(i == timeLimit){
-		clearInterval(timer);
-		loginPopup.classList.add("show");
-	   } 
-	   console.log(i)
-	  },1000);
+function showPopup() {
+  const timeLimit = 3; // seconds;
+  let i = 0;
+  const timer = setInterval(function () {
+    i++;
+    if (i == timeLimit) {
+      clearInterval(timer);
+      loginPopup.classList.add("show");
+    }
+    console.log(i);
+  }, 1000);
 }
 
-
-close.addEventListener("click",function(){
+close.addEventListener("click", function () {
   loginPopup.classList.remove("show");
-})
-
+});
